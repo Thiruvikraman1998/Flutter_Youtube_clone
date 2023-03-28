81891a61810aaa3789259bcbbb365adb10af28ad
Thiruvikraman1998 <development.thiru@gmail.com>
2023-03-28 13:09:04 +0530

    initial commit

---
+++ youtube_clone_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {
+  runApp(const YoutubeClone());
+}
+
+class YoutubeClone extends StatefulWidget {
+  const YoutubeClone({super.key});
+
+  @override
+  State<YoutubeClone> createState() => _YoutubeCloneState();
+}
+
+class _YoutubeCloneState extends State<YoutubeClone> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
